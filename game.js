532945804f29a711f1e1ae54fcf6e68cdae3752d
Luis Aguila <aguilaluis22@gmail.com>
2022-04-14 13:21:37 -0600

    La wea detectada

--- game.js
+++ game.js
@@ -3,7 +3,10 @@ var ctx = canvas.getContext("2d");
 var speed = 10;
 var rightPressed = false;
 var leftPressed = false;
+var touchCube = false;
 var moveAxis = 1;
+var score = 0;
+var lives = 3;
 //move Axis
 /*
 Derecha = 1
@@ -15,19 +18,21 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    if(e.keyCode == 68) {
+    if (e.keyCode == 68) {
         rightPressed = true;
+        movement();
     }
-    else if(e.keyCode == 65) {
+    else if (e.keyCode == 65) {
         leftPressed = true;
+        movement();
     }
 }
 
 function keyUpHandler(e) {
-    if(e.keyCode == 68) {
+    if (e.keyCode == 68) {
         rightPressed = false;
     }
-    else if(e.keyCode == 65) {
+    else if (e.keyCode == 65) {
         leftPressed = false;
     }
 }
@@ -47,11 +52,10 @@ this.height = 10;
 var cabeza = new Cube(this.posX, this.posY, this.width, this.height);
 var cuerpo = new Cube(this.posX, this.posY, this.width, this.height);
 
-
-    cuerpo.posX = Math.floor(Math.random() * canvas.width-10);
-    cuerpo.posY = Math.floor(Math.random() * canvas.height-10);;
-    console.log(cuerpo.posX);
-    console.log(cuerpo.posY);
+cuerpo.posX = Math.floor(Math.random() * canvas.width - 10);
+cuerpo.posY = Math.floor(Math.random() * canvas.height - 10);;
+console.log(cuerpo.posX);
+console.log(cuerpo.posY);
 
 function drawCubes() {
     ctx.beginPath();
@@ -67,48 +71,58 @@ function drawCubes() {
     ctx.closePath();
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCubes();
-
-    if(rightPressed && moveAxis == 1) {
+function movement(){
+    if (rightPressed && moveAxis == 1) {
         moveAxis = 3;
     }
-    else if(leftPressed && moveAxis == 1) {
+    else if (leftPressed && moveAxis == 1) {
         moveAxis = 4;
     }
-    else if(leftPressed && moveAxis == 2) {
+    else if (leftPressed && moveAxis == 2) {
         moveAxis = 3;
     }
-    else if(rightPressed && moveAxis == 2) {
+    else if (rightPressed && moveAxis == 2) {
         moveAxis = 4;
     }
-    else if(rightPressed && moveAxis == 3) {
+    else if (rightPressed && moveAxis == 3) {
         moveAxis = 2;
     }
-    else if(leftPressed && moveAxis == 3) {
+    else if (leftPressed && moveAxis == 3) {
         moveAxis = 1;
     }
-    else if(rightPressed && moveAxis == 4) {
+    else if (rightPressed && moveAxis == 4) {
         moveAxis = 1;
     }
-    else if(leftPressed && moveAxis == 4) {
+    else if (leftPressed && moveAxis == 4) {
         moveAxis = 2;
-    }
-
-
-    if(moveAxis == 1) {
-        cabeza.posX += speed; //Derecha
-    }
-    else if(moveAxis == 2) { //Izquierda
-        cabeza.posX -= speed;
-    }
-    else if(moveAxis == 3) { //Abajo
-        cabeza.posY += speed;
-    }
-    else if(moveAxis == 4) { //Arriba
-        cabeza.posY -= speed; 
     }
 }
 
-draw();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (moveAxis == 1) {
+        cabeza.posX += speed; //Derecha
+    }
+    else if (moveAxis == 2) { //Izquierda
+        cabeza.posX -= speed;
+    }
+    else if (moveAxis == 3) { //Abajo
+        cabeza.posY += speed;
+    }
+    else if (moveAxis == 4) { //Arriba
+        cabeza.posY -= speed;
+    }
+
+    drawCubes();
+    scoreCount();
+}
+
+function scoreCount() {
+    if(cabeza.posX >= cuerpo.posX && cabeza.posX < cuerpo.posX+10 && cabeza.posY >= cuerpo.posY && cabeza.posY < cuerpo.posY+10) {
+        score += 1;
+    }
+    console.log(score);
+}
+
+setInterval(draw, 100);
